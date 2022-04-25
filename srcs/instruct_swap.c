7558aee609cc2e4fed04c3605dd294878963c03e
Théophane P <theodyme@gmail.com>
2022-04-25 15:08:36 +0200

    Add files via upload

--- srcs/instruct_swap.c
+++ srcs/instruct_swap.c
@@ -1,1 +1,10 @@
-l
+/*
+
+to do:
+
+sa (swap a): Swap the first 2 elements at the top of stack a.
+Do nothing if there is only one or no elements.
+sb (swap b): Swap the first 2 elements at the top of stack b.
+Do nothing if there is only one or no elements.
+
+*/
