65a66171157c19604e940b7a5b3e735c2da585a0
Théophane P <theodyme@gmail.com>
2022-05-31 18:14:49 +0200

    Add files via upload

--- main.c
+++ main.c
@@ -1,45 +1,43 @@
 #include "push_swap.h"
 
-void    dispatch(int ac, s_list **a, s_list **b)
+void	dispatch(int ac, s_list **a, s_list **b)
 {
-    if(ac == 3)
-        sort_two(a);
-    else if(ac == 4)
-        sort_three(a);
-    else if(ac == 6)
-        sort_five(a, b);
-    return ;
+	if(ac == 3)
+		sort_two(a);
+	else if(ac == 4)
+		sort_three(a);
+	else if(ac == 6)
+		sort_five(a, b);
+	return ;
 }
 
-int     main(int ac, char **av)
+int		main(int ac, char **av)
 {
-    static struct s_list   *a = NULL;
-    static struct s_list   *b = NULL;
-    int                     cost;
+	static struct s_list	*a = NULL;
+	static struct s_list	*b = NULL;
+	int						cost;
 
-    cost = 0;
-    if(ac == 1)
-        return (0);
-    stackbuilder(av, &a);
-    if (dupchecker(&a) == 1)
-    {
-        return (0);
-    }
-    if ((checker(&a, &b)) == 1)
-        return (1);
-    if (ac == 3 || ac == 4 || ac == 6)
-        dispatch(ac, &a, &b);
-    else
-    {
-        quicksort(&a, &b);
-        if (a->content > a->next->content)
-            i_swap(&a, 'a');
-        while (b != NULL)
-            insertsort(&a, &b);
-        cost = cost_to_top(&a, get_min(&a));
-        rot_to_i(&a, 'a', cost);
-    }
-	stackprinter(&a, 'a');
-	stackprinter(&b, 'b');
-    return (0);
+	cost = 0;
+	if(ac == 1)
+		return (0);
+	stackbuilder(av, &a);
+	if (dupchecker(&a) == 1)
+	{
+		return (0);
+	}
+	if ((checker(&a, &b)) == 1)
+		return (1);
+	if (ac == 3 || ac == 4 || ac == 6)
+		dispatch(ac, &a, &b);
+	else
+	{
+		quicksort(&a, &b);
+		if (a->content > a->next->content)
+			i_swap(&a, 'a');
+		while (b != NULL)
+			insertsort(&a, &b);
+		cost = cost_to_top(&a, get_min(&a));
+		rot_to_i(&a, 'a', cost);
+	}
+	return (0);
 }
--- sort_utilities.c
+++ sort_utilities.c
@@ -2,63 +2,63 @@
 
 int     get_max(s_list **stack)
 {
-    int max;
-    struct s_list   *tmp;
+	int max;
+	struct s_list   *tmp;
 
-    max = (*stack)->content;
-    tmp = (*stack);
-    while (tmp->next != (*stack))
-    {
-        if (max < tmp->content)
-            max = tmp->content;
-        tmp = tmp->next;
-    }
-    if (max < tmp->content)
-        max = tmp->content;
-    return (max);
+	max = (*stack)->content;
+	tmp = (*stack);
+	while (tmp->next != (*stack))
+	{
+		if (max < tmp->content)
+			max = tmp->content;
+		tmp = tmp->next;
+	}
+	if (max < tmp->content)
+		max = tmp->content;
+	return (max);
 }
 
 int     get_min(s_list **stack)
 {
-    int min;
-    struct s_list   *tmp;
+	int min;
+	struct s_list   *tmp;
 
-    min = (*stack)->content;
-    tmp = (*stack);
-    while (tmp->next != (*stack))
-    {
-        if (min > tmp->content)
-            min = tmp->content;
-        tmp = tmp->next;
-    }
-    if (min > tmp->content)
-        min = tmp->content;
-    return (min);
+	min = (*stack)->content;
+	tmp = (*stack);
+	while (tmp->next != (*stack))
+	{
+		if (min > tmp->content)
+			min = tmp->content;
+		tmp = tmp->next;
+	}
+	if (min > tmp->content)
+		min = tmp->content;
+	return (min);
 }
 
 int     get_size(s_list **stack)
 {
-    int size;
-    struct s_list   *tmp;
+	int size;
+	struct s_list   *tmp;
 
-    size = 0;
-    tmp = (*stack);
-    while (tmp->next != (*stack))
-    {
-        size++;
-        tmp = tmp->next;
-    }
-    size++;
-    return (size);
+	size = 0;
+	tmp = (*stack);
+	while (tmp->next != (*stack))
+	{
+		size++;
+		tmp = tmp->next;
+	}
+	size++;
+	return (size);
 }
 
 int     get_med(s_list **stack)
 {
-    int med;
+	int med;
 
-    med = (get_size(stack) / 2);
-    if (((med % 2) != 0) && med != 1)
-        med += 1;
-    return (med);
+	med = (get_size(stack) / 2);
+	if (((med % 2) != 0) && med != 1)
+		med += 1;
+	return (med);
 }
 
