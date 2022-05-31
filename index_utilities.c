364c6e3bcf1fa4bb84c345fbf5d07dbcd78f13fc
Théophane P <theodyme@gmail.com>
2022-05-31 13:54:17 +0200

    Add files via upload

--- index_utilities.c
+++ index_utilities.c
@@ -1,70 +1,65 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   index_utilities.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: flplace <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/31 13:50:44 by flplace           #+#    #+#             */
+/*   Updated: 2022/05/31 13:52:10 by flplace          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "push_swap.h"
 #include <stdlib.h>
 
-// void    set_index(s_list **stack)
-// {
-//     int i;
-//     struct s_list   *tmp;
-
-//     i = 1;
-//     tmp = (*stack);
-//     while (tmp->next != (*stack))
-//     {
-//         tmp->index = i;
-//         i++;
-//         tmp = tmp->next;
-//     }
-//     tmp->index = i;
-//     return ;
-// }
-
-void    sort_index(s_list **stack)
+void	sort_index(s_list **stack)
 {
-    struct s_list   *tmp;
-    int             *tab;
-    int             i;
+	struct s_list	*tmp;
+	int		*tab;
+	int		i;
 
-    i = 0;
-    tab = (int *)ft_calloc(sizeof(int), get_size(stack));
-    tmp = (*stack);
-    fill_tab(tab, stack);
-    sort_tab(tab, (get_size(stack)));
-    while (tmp->next != (*stack))
-    {
-        while (tab[i] && tmp->content != tab[i])
-            i++;
-        tmp->index = i + 1;
-        i = 0;
-        tmp = tmp->next;
-    }
-    while (tab[i] && tmp->content != tab[i])
-        i++;
-    tmp->index = i + 1;
-    free(tab);
-    return ;
+	i = 0;
+	tab = (int *)ft_calloc(sizeof(int), get_size(stack));
+	tmp = (*stack);
+	fill_tab(tab, stack);
+	sort_tab(tab, (get_size(stack)));
+	while (tmp->next != (*stack))
+	{
+		while (tab[i] && tmp->content != tab[i])
+			i++;
+		tmp->index = i + 1;
+		i = 0;
+		tmp = tmp->next;
+	}
+	while (tab[i] && tmp->content != tab[i])
+		i++;
+	tmp->index = i + 1;
+	free(tab);
+	return ;
 }
 
-void    fill_tab(int *tab, s_list **stack)
+void	fill_tab(int *tab, s_list **stack)
 {
-    int i;
-    struct s_list   *tmp;
+	int		i;
+	struct s_list	*tmp;
 
-    i = 0;
-    tmp = (*stack);
-    while (tmp->next != (*stack))
-    {
-        tab[i] = tmp->content;
-        i++;
-        tmp = tmp->next;
-    }
-    tab[i] = tmp->content;
-    return ;
+	i = 0;
+	tmp = (*stack);
+	while (tmp->next != (*stack))
+	{
+		tab[i] = tmp->content;
+		i++;
+		tmp = tmp->next;
+	}
+	tab[i] = tmp->content;
+	return ;
 }
 
 void	sort_tab(int *tab, int size)
 {
-	int i;
-	int tmp;
+	int	i;
+	int	tmp;
 
 	i = 0;
 	while (i < size - 1)
@@ -76,8 +71,8 @@ void	sort_tab(int *tab, int size)
 			tab[i] = tmp;
 			i = 0;
 		}
-        else
-            i++;
+		else
+			i++;
 	}
-    return ;
+	return ;
 }
